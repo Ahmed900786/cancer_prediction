bbab0383dac7ba56212040d14799a16681feee72
Ahmed900786 <ahmedbaimin900@gmail.com>
2024-07-27 00:37:07 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -11,14 +11,14 @@ st.image(r"innomatics_logo.png")
 st.title("Cancer Prediction")
 
 Age = st.number_input("Enter The Age:")
-Gender =st.text_input('Enter The Gender:')
+Gender =st.selectbox('Gender','Male','Female')
 Tumor_Size=st.number_input('Enter The Tumor Size:')
-Tumor_Grade=st.text_input('Enter The Tumor Grade:')
-Symptoms_Severity=st.text_input('Enter The Tumor Symptoms Severity:')
-Family_History = st.text_input('Enter The Family History:')
-Smoking_History= st.text_input('Enter The Smoking History:')
-Alcohol_Consumption=st.text_input('Enter The Alcohol Consumption:')
-Exercise_Frequency = st.text_input('Enter The Excercise Frequency:')
+Tumor_Grade=st.selectbox('Tumor Grade','High,'Low','Medium')
+Symptoms_Severity=st.selectbox('Symptoms Severity','Mild', 'Moderate', 'Severe')
+Family_History = st.selectbox('Family History','Yes','No')
+Smoking_History= st.selectbox('Smoking History','Former Smoker', 'Current Smoker', 'Non-Smoker')
+Alcohol_Consumption=st.selectbox('Alcohol Consumption','Moderate','High','Low')
+Exercise_Frequency = st.selectbox('Exercise Frequency','Regularly', 'Rarely', 'Occasionally', 'Never')
 
 if st.button("Submit"):
     prediction=model.predict([[Age,Gender,Tumor_Size,Tumor_Grade,Symptoms_Severity,Family_History,Smoking_History,Alcohol_Consumption,Exercise_Frequency]])[0]
